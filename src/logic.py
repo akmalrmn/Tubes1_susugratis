904f4c05d38f69c137d1834580619be80762ef7b
akmal <akmalramadannn@gmail.com>
2024-02-28 13:01:40 +0700

    y

--- src/logic.py
+++ src/logic.py
@@ -1,65 +1,69 @@
 import random
 from ..util import get_direction, position_equals
+from typing import List
 
 
 class RandomDiamondLogic(object):
+    def recursive_search(self, board, current_position, visited):
+        if current_position in visited:
+            return False
+        visited.append(current_position)
+        if current_position in board.diamonds:
+            return True
+        if current_position in board.obstacles:
+            return False
+        if current_position.x < 0 or current_position.x > 15 or current_position.y < 0 or current_position.y > 15:
+            return False
+        return self.recursive_search(board, (current_position.x + 1, current_position.y), visited) or self.recursive_search(board, (current_position.x - 1, current_position.y), visited) or self.recursive_search(board, (current_position.x, current_position.y + 1), visited) or self.recursive_search(board, (current_position.x, current_position.y - 1), visited)
+
     def __init__(self):
         self.goal_position = None
         self.previous_position = (None, None)
         self.turn_direction = 1
 
     def next_move(self, board_bot, board):
-        props = board_bot.properties
-        current_position = board_bot.position
-        if props.diamonds == 5:
-            base = props.base
-            self.goal_position = base
-        elif self.goal_position is None or position_equals(
-            current_position, self.goal_position
-        ):
-            # Move towards a random diamond on board
-            tries = len(board.diamonds)
-            index = int(len(board.diamonds) * random.random())
-            while tries > 0:
-              diamond = board.diamonds[index]
-              # Check if we can pick this diamond up before moving to it
-              worth = diamond.properties.points
-              space_left = props.inventory_size - props.diamonds
-              if diamond.properties.points > space_left:
-                # Nope, no space left in inventory. Try another one
-                index = (index + 1) % len(board.diamonds)
-                tries -= 1
-              else:
-                # Ok walk towards this diamond
-                self.goal_position = board.diamonds[index].position
-                break
-
-
-        if self.goal_position:
-            current_position = board_bot.position
-            cur_x = current_position.x
-            cur_y = current_position.y
-
-            # Calculate move according to goal position
-            delta_x, delta_y = get_direction(
-                current_position.x,
-                current_position.y,
-                self.goal_position.x,
-                self.goal_position.y,
-            )
-
-            if (cur_x, cur_y) == self.previous_position:
-                # We did not manage to move, lets take a turn to hopefully get out stuck position
-                if delta_x != 0:
-                    delta_y = delta_x * self.turn_direction
-                    delta_x = 0
-                elif delta_y != 0:
-                    delta_x = delta_y * self.turn_direction
-                    delta_y = 0
-                # Switch turn direction for next time
-                self.turn_direction = -self.turn_direction
-            self.previous_position = (cur_x, cur_y)
-
-            return delta_x, delta_y
-
+        print(board.game_objects)
         return 0, 0
+        # props = board_bot.properties
+        # current_position = board_bot.position
+        # if props.diamonds == 5 and current_position != props.base:
+        #     base = props.base
+        #     self.goal_position = base
+        # elif self.goal_position is None or position_equals(
+        #     current_position, self.goal_position
+        # ):
+        #     total_diamonds = len(board.diamonds)
+        #     worth = 0
+        #     for i in range(total_diamonds):
+        #         diamond = board.diamonds[i]
+        #         point = diamond.properties.points
+        #         delta_x = diamond.position.x - current_position.x
+        #         delta_y = diamond.position.y - current_position.y
+        #         distance = abs(delta_x) + abs(delta_y)
+        #         if current_position == props.base:
+        #             base_distance = 0
+        #             worth = max(worth, point / distance)
+        #         else:
+        #             base_distance = abs(
+        #                 props.base.x - diamond.position.x) + abs(props.base.y - diamond.position.y)
+        #             worth = max(worth, point / (distance + base_distance))
+        #         if worth == point / (distance + base_distance) and diamond.position not in [(15, 0), (0, 15), (0, 0), (15, 15)]:
+        #             if props.diamonds == 4 and point == 2:
+        #                 continue
+        #             self.goal_position = diamond.position
+        # #     base_distance = abs(props.base.x - current_position.x) + \
+        # #         abs(props.base.y - current_position.y)
+        # #     goal_distance = abs(self.goal_position.x - current_position.x) + \
+        # #         abs(self.goal_position.y - current_position.y)
+        # #     if goal_distance > base_distance and current_position != props.base:
+        # #         self.goal_position = props.base
+        # # Calculate move according to goal position
+        # delta_x, delta_y = get_direction(
+        #     current_position.x,
+        #     current_position.y,
+        #     self.goal_position.x,
+        #     self.goal_position.y,
+        # )
+        # self.previous_position = (current_position.x, current_position.y)
+
+        # return delta_x, delta_y
